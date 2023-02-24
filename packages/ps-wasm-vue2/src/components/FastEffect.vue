46f23c490241e39f87f31bc28d48911f7581bc8d
walljack <walljack@163.com>
2023-02-24 09:44:33 +0800

    添加导出逻辑

---
+++ packages/ps-wasm-vue2/src/components/FastEffect.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="effect-list">
+    <!--<div class="effect-item">
+      <el-button @click="fabricBlur">fabric blur</el-button>
+    </div>-->
+    <div class="effect-item" @click="doFilter('gaussBlur')">
+      <img :src="e1" />
+      <span>{{ easyReflection.e1 }}</span>
+    </div>
+    <div class="effect-item">
+      <img :src="e2" />
+      <span>{{ easyReflection.e2 }}</span>
+    </div>
+    <div class="effect-item">
+      <img :src="e3" />
+      <span>{{ easyReflection.e3 }}</span>
+    </div>
+    <div class="effect-item">
+      <img :src="e4" />
+      <span>{{ easyReflection.e4 }}</span>
+    </div>
+    <div class="effect-item">
+      <img :src="e5" />
+      <span>{{ easyReflection.e5 }}</span>
+    </div>
+    <div class="effect-item">
+      <img :src="e6" />
+      <span>{{ easyReflection.e6 }}</span>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import e1 from '../../static/images/effect/e1.png'
+import e2 from '../../static/images/effect/e2.png'
+import e3 from '../../static/images/effect/e3.png'
+import e4 from '../../static/images/effect/e4.png'
+import e5 from '../../static/images/effect/e1.png'
+import e6 from '../../static/images/effect/e1.png'
+const EasyReflection = {
+  e1: '美肤',
+  e2: "素描",
+  e3: "自然增强",
+  e4: "紫调",
+  e5: "柔焦",
+  e6: "复古",
+  e7: "黑白",
+  e8: "仿lomo",
+  e9: "亮白增强",
+  e10: "灰白",
+  e11: "灰色",
+  e12: "暖秋",
+  e13: "木雕",
+  e14: "粗糙"
+}
+const imgs = Object.keys(EasyReflection).map(key => import(`./static/images/effect/${key}.png`))
+export default {
+  data() {
+    return {
+      e1,
+      e2,
+      e3,
+      e4,
+      e5,
+      e6,
+      imgs,
+      easyReflection: EasyReflection
+    }
+  },
+  mounted() {
+    // console.log(this)
+  },
+  methods: {
+    fabricBlur() {
+      this.$emit('fabric-filter', 'blur')
+    },
+    doFilter(type) {
+      this.$emit('do-filter', type)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.effect-list {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+}
+.effect-item {
+  width: 80px;
+  display: flex;
+  cursor: pointer;
+  flex-direction: column;
+  span {
+    font-size: 12px;
+  }
+  img {
+    width: 100%;
+    margin: 4px;
+  }
+}
+</style>
--- packages/ps-wasm-vue2/src/stage.vue
+++ packages/ps-wasm-vue2/src/stage.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="stage">
     <el-container>
-      <el-aside width="200px">
+      <el-aside width="200px" class="left-function">
+        <el-tabs v-model="activeName" type="card" class="left-function-tabs">
+          <el-tab-pane label="特效" name="fastEffect">
+            <FastEffect @fabric-filter="onFabricFilter" @do-filter="doFilter" />
+          </el-tab-pane>
+          <el-tab-pane label="调整" name="ttttt">
+            <FastEffect />
+          </el-tab-pane>
+        </el-tabs>
 
       </el-aside>
       <el-container>
-        <el-header>
-          <el-upload action="" :auto-upload="false" :show-file-list="false" :on-change="onFileAdd">
-            <el-button slot="trigger" size="small" type="primary">选取文件</el-button>
+        <el-header height="32px">
+          <el-upload action="" :auto-upload="false" :show-file-list="false" :on-change="onFileAdd" class="import-file">
+            <el-button slot="trigger" size="mini" type="primary">选取文件</el-button>
           </el-upload>
+          <el-button v-if="showExport" size="mini" type="primary" @click="onExport">导出</el-button>
         </el-header>
-        <el-main>
-          <canvas id="imgRect" />
-          <div class="painting" />
+        <el-main ref="main" class="main-stage">
+          <canvas ref="imgRect" :width="width" :height="height" />
         </el-main>
         <el-footer>
 
@@ -23,14 +31,24 @@
 </template>
 
 <script>
-import { addImage } from "./utils"
-import { fabric } from './lib/fabric.min'
+import {addImage} from "./utils"
+import {fabric} from './lib/fabric.min'
 import {loadBlobWebAssembly} from "./wasmHelper"
 import add from './lib/add.wasm'
+import FastEffect from './components/FastEffect.vue'
+import {GaussBlur} from "./filters/GaussBlur"
+import Canvas2Image from "./CanvasToImage";
+
+fabric.enableGLFiltering = false
 
 export default {
+  components: { FastEffect },
   data() {
     return {
+      width: 300,
+      height: 300,
+      currentObject: null,
+      activeName: 'fastEffect',
       canvas: null
     }
   },
@@ -38,34 +56,81 @@ export default {
     loadBlobWebAssembly(add).then(instance => {
       console.warn('测试 wasm 方法（add），333 + 555 =', instance.exports.add(333, 555))
     })
-    const canvas = new fabric.Canvas('imgRect');
-    const rect = new fabric.Rect({
-      left: 100,
-      top: 50,
-      fill: 'yellow',
-      width: 200,
-      height: 100,
-      objectCaching: false,
-      stroke: 'lightgreen',
-      strokeWidth: 4,
+    // const rect = new fabric.Rect({
+    //   left: 100,
+    //   top: 50,
+    //   fill: 'yellow',
+    //   width: 200,
+    //   height: 100,
+    //   objectCaching: false,
+    //   stroke: 'lightgreen',
+    //   strokeWidth: 4,
+    // })
+    // canvas.add(rect)
+    // canvas.setActiveObject(rect)
+    this.refreshSize()
+    this.$nextTick(() => {
+      this.canvas = new fabric.Canvas(this.$refs.imgRect)
+      console.log(fabric, this.canvas)
     })
-
-    canvas.add(rect)
-    canvas.setActiveObject(rect)
-
-    this.canvas = canvas
+  },
+  computed: {
+    showExport() {
+      return !!this.canvas
+    }
   },
   methods: {
+    refreshSize() {
+      if (this.$refs.main) {
+        const { width, height } = this.$refs.main.$el.getBoundingClientRect()
+        this.width = width
+        this.height = height
+        console.log(width, height)
+      }
+    },
     onFileAdd(file) {
       const reader = new FileReader();
       reader.readAsDataURL(file.raw);
-      reader.onload = function() {
-        const img = new Image()
-        img.src = this.result
-        img.onload = function(){
-          addImage(img)
+      reader.onload = e => {
+        // const img = new Image()
+        // img.src = this.result
+        // img.onload = function(){
+        //   addImage(img)
+        // }
+        fabric.Image.fromURL(e.target.result, img => {
+          this.canvas.add(img)
+          img.on('selected', () => {
+            this.currentObject = img
+          })
+        })
+      }
+    },
+
+    onFabricFilter(type) {
+      switch (type) {
+        case 'blur':
+          const filter = new fabric.Image.filters.Blur({ webgl: false,
+            blur: 0.5
+          });
+          this.currentObject.filters.push(filter)
+          this.currentObject.applyFilters()
+          this.canvas.renderAll()
+      }
+    },
+
+    doFilter(filter) {
+      switch (filter) {
+        case 'gaussBlur': {
+          const gaussBlur = new GaussBlur({ webgl: false })
+          this.currentObject.filters.push(gaussBlur)
+          this.currentObject.applyFilters()
+          this.canvas.renderAll()
         }
       }
+    },
+
+    onExport() {
+      Canvas2Image.saveAsPNG(this.canvas.toCanvasElement(), this.width, this.height)
     }
   },
 }
@@ -75,7 +140,28 @@ export default {
 .stage {
   border: 1px solid #dedede;
 }
+.left-function {
+  border-right: 1px solid #dedede;
+}
+.left-function-tabs {
+  padding-top: 12px;
+  ::v-deep .el-tabs__item {
+    height: 26px;
+    line-height: 26px;
+  }
+}
 .el-aside {
-  background-color: #dedede;
+}
+.el-header {
+  padding: 6px 8px;
+  text-align: left;
+}
+.main-stage {
+  padding: 0;
+  margin: 8px;
+  border: 1px solid #dedede;
+}
+.import-file {
+  display: inline-block;
 }
 </style>
