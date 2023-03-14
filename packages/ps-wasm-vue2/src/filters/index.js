7c2fa2db43c3557f6cbc5ffc2a68cd39a62713cf
walljack <walljack@163.com>
2023-03-14 17:04:48 +0800

    改为使用photon_rs_bg

--- packages/ps-wasm-vue2/src/filters/index.js
+++ packages/ps-wasm-vue2/src/filters/index.js
@@ -1,21 +1,21 @@
 import {loadBlobWebAssembly} from "@/utils/wasmHelper"
-// import imageProcess from "@/lib/photonCustom/photon_rs_bg.wasm"
-// import * as wasmImportJs from "@/lib/photonCustom/photon_rs_bg.js"
-//
-// let wasmInstance
-// loadBlobWebAssembly(imageProcess, {'./photon_rs_bg.js': wasmImportJs}).then(instance => {
-//   wasmInstance = instance
-//   wasmImportJs.__wbg_set_wasm(instance.exports)
-// })
-
-import imageProcess from "@/lib/photon_bg.wasm"
-import * as wasmImportJs from "@/lib/photon.js"
+import imageProcess from "@/lib/photonCustom/photon_rs_bg.wasm"
+import * as wasmImportJs from "@/lib/photonCustom/photon_rs_bg.js"
 
 let wasmInstance
-loadBlobWebAssembly(imageProcess, {'./photon': wasmImportJs}).then(instance => {
+loadBlobWebAssembly(imageProcess, {'./photon_rs_bg.js': wasmImportJs}).then(instance => {
   wasmInstance = instance
   wasmImportJs.__wbg_set_wasm(instance.exports)
 })
+
+// import imageProcess from "@/lib/photon_bg.wasm"
+// import * as wasmImportJs from "@/lib/photon.js"
+//
+// let wasmInstance
+// loadBlobWebAssembly(imageProcess, {'./photon': wasmImportJs}).then(instance => {
+//   wasmInstance = instance
+//   wasmImportJs.__wbg_set_wasm(instance.exports)
+// })
 
 
 /**
